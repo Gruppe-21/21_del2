fe41d467bce9d7b9e57db05f3a6037e758cc0549
Hildibjørg <s164539@student.dtu.dk>
2020-10-09 14:28:17 +0200

    Oversat tidligere spiller klasse

--- src/main/java/com/gruppe21/Player.java
+++ src/main/java/com/gruppe21/Player.java
@@ -1,4 +1,51 @@
 package com.gruppe21;
 
 public class Player {
+
+
+    private String name = "";            // spillers navn
+    private int point = 0;               // spillers point
+    //private boolean toSekserer = false;  // tjekker om spiller har to seksere
+    private boolean navnSlutterMedS;     // tjekker om navn slutter med s
+
+    public Player(String name){
+        setName(name);
+    }
+    public Player(){
+    }
+
+    // hent point værdi
+    public int getPoint() {
+        return point;
+    }
+
+    // sætter spillers point til givne int
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+
+    //public boolean isToSekserer() {return toSekserer;}
+
+    //public void setToSekserer(boolean toSekserer) {this.toSekserer = toSekserer;}
+
+    // hent navn som string
+    public String getName() {
+        return name;
+    }
+
+    // sæt navn
+    public void setName(String name) {
+        this.name = name;
+        navnSlutterMedS = getName().toLowerCase().endsWith("s");
+    }
+
+    public boolean isNavnSlutterMedS() {
+        return navnSlutterMedS;
+    }
+
+    // tilføj point
+    public void addPoint(int i){
+        setPoint(getPoint() + i);
+    }
 }
