eea8de61e2ae9b1ce01af828265a450d9cbbdc16
s205436 <s205436@student.dtu.dk>
2020-10-21 16:10:40 +0200

    ændring i kommentar

--- src/test/java/com/gruppe21/BankBalanceTest.java
+++ src/test/java/com/gruppe21/BankBalanceTest.java
@@ -8,7 +8,7 @@ public class BankBalanceTest {
     @Test
     public void addBankBalanceTest(){
         BankBalance bankBalance = new BankBalance(); // make instance of BankBalance called bankBalance
-        bankBalance.setBankBalance(3000);            // BankBalance set to 3000
+        bankBalance.setBankBalance(3000);            // bankBalance set to 3000
         int amount = 100;
 
         int currentBalance = bankBalance.addBankBalance(amount); // call addBankBalance on bankBalance
@@ -19,7 +19,7 @@ public class BankBalanceTest {
     @Test
     public void removeBankBalanceTest(){
         BankBalance bankBalance = new BankBalance();
-        bankBalance.setBankBalance(3000);           // BankBalance set to 3000
+        bankBalance.setBankBalance(3000);           // bankBalance set to 3000
         int amount = -100;
 
         int currentBalance = bankBalance.removeBankBalance(amount); // call removeBankBalance on bankBalance
