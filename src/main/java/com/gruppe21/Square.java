6cd88e628d3f8f3cff49ed708dfe287d76f5ffd3
Tobias Maneschijn <s205422@student.dtu.dk>
2020-10-21 20:39:08 +0200

    inital implementation of Square

--- src/main/java/com/gruppe21/Square.java
+++ src/main/java/com/gruppe21/Square.java
@@ -1,4 +1,46 @@
 package com.gruppe21;
 
 public class Square {
+
+    String name;
+    String eventText;
+    int modifyValue;
+
+    Square(String _name, String _eventText, int _modifyValue) {
+        this.name = _name;
+        this.eventText = _eventText;
+        this.modifyValue = _modifyValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEventText() {
+        return eventText;
+    }
+
+    public void setEventText(String eventText) {
+        this.eventText = eventText;
+    }
+
+    public int getModifyValue() {
+        return modifyValue;
+    }
+
+    public void setModifyValue(int modifyValue) {
+        this.modifyValue = modifyValue;
+    }
+
+
+    public void handleEvent(Player player) {
+        System.out.println(eventText);
+        //TODO add modifyValue to bankBalance
+    }
+
+
 }
