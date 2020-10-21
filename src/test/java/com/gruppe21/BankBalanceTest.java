a9494a792850e5d198568c83beb7c4fbcd6ae459
s205436 <s205436@student.dtu.dk>
2020-10-21 17:32:06 +0200

    BankBalanceTest extends BankBalance

--- src/test/java/com/gruppe21/BankBalanceTest.java
+++ src/test/java/com/gruppe21/BankBalanceTest.java
@@ -3,7 +3,7 @@ package com.gruppe21;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class BankBalanceTest {
+public class BankBalanceTest extends BankBalance {
 
     @Test
     public void addBankBalanceTest(){
