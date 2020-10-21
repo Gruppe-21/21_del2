a9d2a6a8f76c2a1167ced46883d09edd3e8e44b8
s205436 <s205436@student.dtu.dk>
2020-10-21 14:28:04 +0200

    Test for remove og add påbegyndt

--- src/test/java/com/gruppe21/BankBalanceTest.java
+++ src/test/java/com/gruppe21/BankBalanceTest.java
@@ -1,7 +1,21 @@
 package com.gruppe21;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class BankBalanceTest extends BankBalance {
+
+    @Test
+    public void addBankBalance(){
+
+    }
+
+
+    @Test
+    public void removeBankBalance(){
+
+    }
+
+
 
 }
