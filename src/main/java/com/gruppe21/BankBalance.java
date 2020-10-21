56afd5c5c8497ac571a8b11d8ab85592901863b8
s205436 <s205436@student.dtu.dk>
2020-10-21 13:44:36 +0200

    getBankBalance() lavet

--- src/main/java/com/gruppe21/BankBalance.java
+++ src/main/java/com/gruppe21/BankBalance.java
@@ -1,4 +1,12 @@
 package com.gruppe21;
 
 public class BankBalance {
+
+    private int balance = 0; // Player has 3000 as starting balance
+
+
+    public int getBankBalance(){
+        return balance;
+    }
+
 }
