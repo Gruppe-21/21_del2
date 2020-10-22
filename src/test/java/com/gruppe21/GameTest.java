5f7efcfd1e0a70fc9b983aabcf53c1302c1d8f98
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-10-22 16:31:54 +0200

    Add test playRoundPlayerCanWin()

--- src/test/java/com/gruppe21/GameTest.java
+++ src/test/java/com/gruppe21/GameTest.java
@@ -1,7 +1,25 @@
 package com.gruppe21;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
+
+
+    @Test
+    public void playRoundPlayerCanWin() {
+        Player[] players = new Player[] {new Player("Player1"), new Player("Player2")};
+        Die[] dice = new Die[] {new Die(), new Die()};
+        Game game = new Game(players, dice, true);
+
+        players[0].getBankBalance().setBalance(2999);
+        dice[0].setValue(1);
+        dice[1].setValue(1);
+        Assert.assertEquals(true, game.playRound());
+        Assert.assertEquals(players[0], game.getPlayers()[game.getCurrentPlayer()]);
+    }
+
 
 }
