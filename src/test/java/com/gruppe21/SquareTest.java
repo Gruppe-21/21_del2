563f68099e61960c3ecc105f5c244d0480addb1e
Tobias Maneschijn <s205422@student.dtu.dk>
2020-10-22 10:57:04 +0200

    Testing of Board and Square

--- src/test/java/com/gruppe21/SquareTest.java
+++ src/test/java/com/gruppe21/SquareTest.java
@@ -9,15 +9,15 @@ public class SquareTest {
 
     @Test
     public void squareReturnsEventText() {
-        Square sq = new Square("place", "Hello World!", 1000);
+        Square sq = new Square("place", "Hello World!", 1000, SquareType.Normal);
 
-        Assert.assertEquals("Hello World!", sq.eventText);
+        Assert.assertEquals("Hello World!", sq.getEventText());
     }
 
     @Test
     public void squareModifiesPlayerBalance() {
         Player player = new Player();
-        Square sq = new Square("place", "Hello World!", 1000);
+        Square sq = new Square("place", "Hello World!", 1000, SquareType.Normal);
 
         // TODO implement test for checking if square modifies a players bank balance
     }
