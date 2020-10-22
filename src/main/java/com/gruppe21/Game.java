fb7e76a585c672fa7dab94eb29e63ebe8008e2e0
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-10-22 12:46:31 +0200

    Initial commit

--- src/main/java/com/gruppe21/Game.java
+++ src/main/java/com/gruppe21/Game.java
@@ -1,4 +1,34 @@
 package com.gruppe21;
 
 public class Game {
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public Die[] getDice() {
+        return dice;
+    }
+
+    public void setDice(Die[] dice) {
+        this.dice = dice;
+    }
+
+    private Player[] players;
+    private Die[] dice;
+
+
+    public Game(Player[] players) {
+        this.players = players;
+        dice = new Die[] {new Die(), new Die()};
+    }
+
+    public Game(Player[] players, Die[] dice) {
+        this.players = players;
+        this.dice = dice;
+    }
 }
