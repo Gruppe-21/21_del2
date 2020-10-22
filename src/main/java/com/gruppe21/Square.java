bfca35e3bc0c74ed3bfdf4be61c2eaa83fc6cc6f
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-10-22 14:28:42 +0200

    Added getter for squareType

--- src/main/java/com/gruppe21/Square.java
+++ src/main/java/com/gruppe21/Square.java
@@ -10,6 +10,8 @@ public class Square {
     private String name;
     private String eventText;
     private int modifyValue;
+
+
     private SquareType squareType;
 
     Square(String _name, String _eventText, int _modifyValue, SquareType _squareType) {
@@ -19,29 +21,19 @@ public class Square {
         this.squareType = _squareType;
     }
 
-    public String getName() {
-        return name;
-    }
+    public SquareType getSquareType() { return squareType; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() { return name; }
 
-    public String getEventText() {
-        return eventText;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setEventText(String eventText) {
-        this.eventText = eventText;
-    }
+    public String getEventText() { return eventText; }
 
-    public int getModifyValue() {
-        return modifyValue;
-    }
+    public void setEventText(String eventText) { this.eventText = eventText; }
 
-    public void setModifyValue(int modifyValue) {
-        this.modifyValue = modifyValue;
-    }
+    public int getModifyValue() { return modifyValue; }
+
+    public void setModifyValue(int modifyValue) { this.modifyValue = modifyValue; }
 
 
     public void handleEvent(Player player) {
