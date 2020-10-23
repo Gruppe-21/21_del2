f528de6fcb76a984640e2e352cb4ae7da2f40cad
Hildibjørg <s164539@student.dtu.dk>
2020-10-23 13:59:59 +0200

    Player tests oprettet

--- src/test/java/com/gruppe21/PlayerTest.java
+++ src/test/java/com/gruppe21/PlayerTest.java
@@ -1,7 +1,51 @@
 package com.gruppe21;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    @Test
+    public void addPlayerNameTest() {
+        Player player = new Player();  // make instance of Player called player
+        player.setName("Bob");
 
-}
+        assertEquals("Bob", player.getName());
+    }
+
+    @Test
+    public void nameWithoutSTest() {
+        Player player = new Player();  // make instance of Player called player
+        player.setName("Bob");
+
+        assertEquals(false, player.isNameEndsWithS());
+    }
+
+    @Test
+    public void changeNameTest() {
+        Player player = new Player();  // make instance of Player called player
+        player.setName("Bob");
+        player.setName("Clara");
+
+        assertEquals("Clara", player.getName());
+    }
+
+    @Test
+    public void nameWithSTest() {
+        Player player = new Player();  // make instance of Player called player
+        player.setName("Louis");
+
+
+        assertEquals(true, player.isNameEndsWithS());
+    }
+    @Test
+    public void changeNameContainsSTest() {
+        Player player = new Player();  // make instance of Player called player
+        player.setName("Bob");
+        player.setName("Louis");
+
+
+        assertEquals(true, player.isNameEndsWithS());
+    }
+
+    }
