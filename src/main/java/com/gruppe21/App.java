e657060aa578abda2323386c48ab6d68a99c2b51
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-10-23 12:38:42 +0200

    Game is created and started in main

--- src/main/java/com/gruppe21/App.java
+++ src/main/java/com/gruppe21/App.java
@@ -6,10 +6,9 @@ package com.gruppe21;
  */
 public class App 
 {
-    /*
     public static void main( String[] args )
     {
-        Game game = new Game(new Player[] {new Player(), new Player()});
-        System.out.println( "Hello World!" );
-    }*/
+        Game game = new Game(new Player[] {new Player("Bob Dylan"), new Player("Henning DiCaprio")});
+        game.startGame();
+    }
 }
