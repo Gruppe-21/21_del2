aa49eda020477758b541632105f70fa42bdd32ce
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-10-28 15:12:18 +0100

    Added unit tests for RandomNameGenerator

---
+++ src/test/java/com/gruppe21/utils/RandomNameGeneratorTest.java
@@ -0,0 +1,45 @@
+package com.gruppe21.utils;
+
+import com.gruppe21.Board;
+import com.gruppe21.Player;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class RandomNameGeneratorTest {
+
+    @Test
+    public void getNameDifferentFromStringsReturnDifferentStrings() {
+        String[] allNames = RandomNameGenerator.getAllNames();
+        String[] names = Arrays.copyOfRange(allNames, 1, allNames.length);
+        Boolean onlyDifferent = true;
+        for (int i = 0; i < 1000000; i++) {
+            if (!RandomNameGenerator.GetNameDifferentFrom(names).equals(allNames[0])) {
+                onlyDifferent = false;
+                break;
+            }
+        }
+        Assert.assertEquals(true, onlyDifferent);
+    }
+
+    @Test
+    public void getNameDifferentFromPlayersReturnDifferentStrings() {
+        String[] allNames = RandomNameGenerator.getAllNames();
+        String[] names = Arrays.copyOfRange(allNames, 1, allNames.length);
+        Player[] players = new Player[names.length];
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new Player(names[i]);
+        }
+        Boolean onlyDifferent = true;
+        for (int i = 0; i < 1000000; i++) {
+            if (!RandomNameGenerator.GetNameDifferentFrom(players).equals(allNames[0])) {
+                onlyDifferent = false;
+                break;
+            }
+        }
+        Assert.assertEquals(true, onlyDifferent);
+    }
+}
